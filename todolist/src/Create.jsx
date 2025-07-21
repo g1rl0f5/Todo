2ee069a25dc98f2a25c4aad2fba0c6d5edea187d
g1rl0f5 <anjithasuresh2001@gmail.com>
2025-07-21 21:46:03 +0530

    Initial commit

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Create() {
+  const [task, setTask] = useState('');
+  const [error, setError] = useState('');
+
+  const handleAdd = () => {
+    if (!task.trim()) {
+      setError('Task cannot be empty');
+      return;
+    }
+
+    axios.post('http://localhost:3001/add', { task })
+      .then(() => {
+        setTask('');
+        setError('');
+        location.reload();
+      })
+      .catch(err => {
+        console.log(err);
+        setError('Something went wrong. Try again.');
+      });
+  };
+
+  return (
+    <div className='create_form'>
+      <input
+        type="text"
+        placeholder='Enter Your Task'
+        value={task}
+        onChange={(e) => {
+          setTask(e.target.value);
+          if (error) setError(''); // clear error while typing
+        }}
+      />
+      <button type="button" onClick={handleAdd}>Add</button>
+      {error && <p className="error-message">{error}</p>}
+    </div>
+  );
+}
+
+
+
+export default Create
